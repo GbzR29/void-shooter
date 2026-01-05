476e38912e5bc17a3b0b25ad74cc1fb831231b3a
Su4amp <gabrielfeliperc@hotmail.com>
2026-01-04 21:52:55 -0300

    added build by cmake

---
+++ build/CMakeFiles/void-shooter.dir/cmake_clean.cmake
@@ -0,0 +1,21 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/void-shooter.dir/src/core/entities/enemy/enemy.cpp.obj"
+  "CMakeFiles/void-shooter.dir/src/core/entities/enemy/enemy.cpp.obj.d"
+  "CMakeFiles/void-shooter.dir/src/core/entities/player/player.cpp.obj"
+  "CMakeFiles/void-shooter.dir/src/core/entities/player/player.cpp.obj.d"
+  "CMakeFiles/void-shooter.dir/src/core/environment/stars.cpp.obj"
+  "CMakeFiles/void-shooter.dir/src/core/environment/stars.cpp.obj.d"
+  "CMakeFiles/void-shooter.dir/src/core/main.cpp.obj"
+  "CMakeFiles/void-shooter.dir/src/core/main.cpp.obj.d"
+  "CMakeFiles/void-shooter.dir/src/core/ui/ui.cpp.obj"
+  "CMakeFiles/void-shooter.dir/src/core/ui/ui.cpp.obj.d"
+  "libvoid-shooter.dll.a"
+  "void-shooter.exe"
+  "void-shooter.exe.manifest"
+  "void-shooter.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/void-shooter.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
